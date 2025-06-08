899cc1634846802471ebedd3056fa1b1fb9537aa
_Diego_Rodriguez_ <dirodriguezd@unal.edu.co>
2025-06-08 11:41:43 -0500

    chore: add sample images, clean up comments, and include shadcn UI components
    feat: add sample images to /public/assets
    refactor: remove unnecessary comments from landing page components
    feat: add shadcn UI components (select, calendar, avatar, card)

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,7 +3,6 @@ import Hero from "../_components/landingPage/Hero";
 
 export default async function Home() {
   return (
-    /* El HydrateClient es un componente de tRPC, que ayuda al SSR */
     <HydrateClient>
       <main>
         <Hero />
