59efe09664ce134dd72bee90ef54e100030f3d02
cesarALS <arthurolemos4@gmail.com>
2025-07-05 03:14:20 -0500

    feature (graphql requests) - new apollo link created, which saves the headers of the response into a record - The record has a UUID that identifies the request, UUID created when the request initiates and saved into it's context

--- src/graphql/callGraphql.ts
+++ src/graphql/callGraphql.ts
@@ -3,35 +3,61 @@
  */
 
 import { ApolloError, type DocumentNode } from "@apollo/client";
+import { randomUUID } from "crypto";
 import type { GraphQLError } from "graphql/error/GraphQLError";
-import { serverClient } from "~/graphql/apolloClient";
+import { GraphQLHeaders, serverClient } from "~/graphql/apolloClient";
 
-interface GraphQLResponse<T> {
-  data?: T;
-  errors?: GraphQLError[];
+interface ServerResponse<T> {
+  data?: T
+  errors?: GraphQLError[]
+}
+
+interface GraphQLResponse<T> extends ServerResponse<T>{
+  headers?: Record<string, string>;
 }
 
 /**
  *
- * @param req       A GraphQL DocumentNode, which has your request
- * @param mutation  A boolean that indicates whether your GraphQL request is a mutation or not (which means its a query)
- * @returns         EThe result of API call, which contains some data an possible errors (even if it was succesful, sometimes)
+ * @param req               A GraphQL DocumentNode, which has your request
+ * @param mutation          A boolean that indicates whether your GraphQL request is a mutation or not (which means its a query)
+ * @param variables
+ * @param includeHeaders    If we want to capture headers sent by the backend, we should include their name here
+ * @returns                 The result of API call, which contains some data, possible errors (even if it was succesful, sometimes), and headers (if specified)
  */
 
 export const callGraphqlAPI = async<T> (
     req:        DocumentNode,
     mutation =  false,
-    variables:  Record<string, unknown> = {}
+    variables:  Record<string, unknown> = {},
 ): Promise<GraphQLResponse<T>> => {
 
     console.log("Starting GraphQL request");
 
     let result = undefined;
 
+    const context = {
+        reqId: randomUUID()
+    }
+
     try {
 
-        if (!mutation)  { result = await serverClient.query( {query: req, fetchPolicy: "no-cache"})                         }
-        else            { result = await serverClient.mutate({mutation: req, variables: variables, fetchPolicy: "no-cache"})}
+        if (!mutation)  {
+            result = await serverClient.query<ServerResponse<T>>({
+                query: req,
+                fetchPolicy: "no-cache",
+                context
+            })
+        } else {
+            result = await serverClient.mutate<ServerResponse<T>>({
+                mutation: req,
+                variables: variables,
+                fetchPolicy: "no-cache",
+                context
+            })
+        }
+
+        const headers = GraphQLHeaders[context.reqId];
+        console.log(headers)
 
         return { data: result.data as T };
 
